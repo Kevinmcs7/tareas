8716a1083f4a83fa5139e987940ade8bf89613f4
Kevin <kevinmgue@gmail.com>
2024-12-12 13:31:22 -0600

    cambios de env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import app from "./app.js";
-import { connectDB } from "./db.js";
+import dotenv from 'dotenv';
 
+// Cargar las variables de entorno
+dotenv.config();
 
+import express from 'express'; // Si usas Express, por ejemplo
+import { connectDB } from './db'; // Asumimos que tienes el archivo db.js
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Conectar a la base de datos
 connectDB();
 
-app.listen(3000)
-console.log('Server on port', 3000)
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
